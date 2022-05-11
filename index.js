4c75ea7e7194d3cc2bbc1a57e88533e2af369ec4
hankeliu2015 <hanke.liu@gmail.com>
2022-05-11 11:58:05 -0400

    testing basic operation logics - wip

--- index.js
+++ index.js
@@ -13,11 +13,34 @@ const calc = function(arr) {
     }
     console.log(numArr, operArr)
     // check if the the operArr length and perform arithmetic operations  
-    // while(operArr.length !== 0) {
+    while(operArr.length !== 0) {
+        let currentOper = operArr[0]
+        let newLastEl = 0
+        // if the last 2 number arr els are true , perform the operation
+        if(numArr.length >= 2) {
+            // perform the action here
+            switch (currentOper) {
+                case '+': 
+                newLastEl = numArr[numArr.length - 2] + numArr[numArr.length - 1]
+                case '-': 
+                newLastEl = numArr[numArr.length - 2] - numArr[numArr.length - 1]
+                case '*': 
+                newLastEl = numArr[numArr.length - 2] * numArr[numArr.length - 1]
+                case '/': 
+                newLastEl = numArr[numArr.length - 2] / numArr[numArr.length - 1]
+            }
+            // replace the last 2 els with newLastEl to num arr
+            numArr.splice(-2, 2, newLastEl)
+            // remove the first el from operArr
+            operArr.unshift()
+            console.log(numArr[numArr.length - 1])
+        } 
+        // else {
+        //     throw new Error('can not perform arithmetic operation, numbers input much match the operators')
+        // }
+    }
 
-    // }
     
-
 }
 console.log(calc([5,5,5,8,'+','+','-']))
 // console.log(calc('5 5 5 8 + + -'))
