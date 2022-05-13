a0933d97e7dae06bd85d9b9c96f2aa59e0b8a503
hankeliu2015 <hanke.liu@gmail.com>
2022-05-12 22:40:46 -0400

    rename variable newLastNumEl

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const readline = require('readline')
 const numArr = [], operArr = []
 
 const calcLogic = function(inputStr) {
+    //convert the input string to arr. 
     const inputArr = inputStr.split(' ')
     console.log('cmd-line input convert to array:',inputArr)
-    //convert the input string to arr. 
     // filter arr els into number array and operator array
     for (let el of inputArr) {
         if(parseInt(el)) {
@@ -12,22 +12,22 @@ const calcLogic = function(inputStr) {
         } else if (el === '+' || el === '-' || el === '*' || el === '/') {
             operArr.push(el)
         } else {
-            throw new Error('Input data must be valid number or operator')
+            throw new Error('Input data must be valid number or operators')
         }
     }
-    console.log('converted to 2 arrays | number array:', numArr, 'Operator array:', operArr)
-
-    let newLastNumEl = numArr[numArr.length - 1]
+    console.log('converted arrays | number array:', numArr, 'Operator array:', operArr)
+    
+    let operResult = numArr[numArr.length - 1]
 
     // if there are only numbers input without any operators, not perform arithmetic operation
     if(operArr.length === 0) {
-        // output the last number in the numArr
-        console.log('calc output for single number:',newLastNumEl)
-
+        // output the current input number, which is the last el in the numArr
+        console.log(operResult)
         // when operators added in arguments  
     } else if(operArr.length >= 1) {
         // when operArr has at least one el, perform operation for the last 2 numArr els
         // need data validation and throw errors for numbers and operators mismatch - wip
+
         while(operArr.length > 0) {
             // obtain the first operator and remove it from the operArr
             let currentOper = operArr.shift()
@@ -35,16 +35,16 @@ const calcLogic = function(inputStr) {
             if(numArr.length >= 2) {
                 switch (currentOper) {
                     case '+': 
-                    newLastNumEl = numArr[numArr.length - 2] + numArr[numArr.length - 1]
+                    operResult = numArr[numArr.length - 2] + numArr[numArr.length - 1]
                     break;
                     case '-': 
-                    newLastNumEl = numArr[numArr.length - 2] - numArr[numArr.length - 1]
+                    operResult = numArr[numArr.length - 2] - numArr[numArr.length - 1]
                     break;
                     case '*': 
-                    newLastNumEl = numArr[numArr.length - 2] * numArr[numArr.length - 1]
+                    operResult = numArr[numArr.length - 2] * numArr[numArr.length - 1]
                     break;
                     case '/': 
-                    newLastNumEl = numArr[numArr.length - 2] / numArr[numArr.length - 1]
+                    operResult = numArr[numArr.length - 2] / numArr[numArr.length - 1]
                     // break;
                     // default: 
                     // throw new Error(`arithmetic operator ${currenOper} is not included in this app`)
@@ -53,14 +53,14 @@ const calcLogic = function(inputStr) {
                 // if there still any els left in numArr, add the new last number el back into numArr
                 numArr.splice(-2, 2)
                 if(numArr.length > 0) {
-                    numArr.push(newLastNumEl)
+                    numArr.push(operResult)
                 } 
             } 
         }
     }
 
     console.log('after operation | Numbers array:', numArr, 'Operators array:', operArr)
-    console.log('calc output:',newLastNumEl)
+    console.log('calc output:',operResult)
 }
 // console.log(calcLogic('5 5 +'))
 // console.log(calcLogic('5 5 5 8 + + -'))
@@ -72,11 +72,11 @@ const rl = readline.createInterface({
     output: process.stdout
   });
 
-const calcInputTest2 = async function() {
+const calcInputLoop = async function() {
     for await ( let cmdLineInput of rl) {
         if ( cmdLineInput === 'q') { break }
         calcLogic(cmdLineInput)
     }
 }
 
-calcInputTest2()
+calcInputLoop()
