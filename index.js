49d0668b2a1ec240d2104147a223510e5c3740be
hankeliu2015 <hanke.liu@gmail.com>
2022-05-17 00:00:44 -0400

    add inputStrValidation function to validate input data

--- index.js
+++ index.js
@@ -1,22 +1,53 @@
-const readline = require('readline')
-let numArr = [], operArr = []
+// const inputStrValidation = require('./utilities/inputStrValidataion')
 
-// after validate and convert the input string inth currentNumArr and currentOperArr. 
-const calcLogic = function(inputStr) {
-    /* 
-    const inputArr = inputStr.split(' ')
-    console.log('cmd-line input convert to array:',inputArr)
-    for (let el of inputArr) {
-        if(parseInt(el)) {
-            numArr.push(parseInt(el))
-        } else if (el === '+' || el === '-' || el === '*' || el === '/') {
-            operArr.push(el)
+const readline = require('readline')
+let numArr = []
+let operArr = []
+let currentNumArr = [] 
+let currentOperArr = []
+
+const inputStrValidation = function(cmdLineStr) {
+    // if str operators ahead of numbers in the string, ask user to re-type
+    // need counter to track when the numbers trun into operator 
+    let operInputStarted = false
+    // need the index 
+    for( let i = 0; i < cmdLineStr.length; i ++) {
+        // how to know if the operInput started? 
+        let currentChar = cmdLineStr.charAt(i)
+
+        if (currentChar === '+' || currentChar === '-' || currentChar === '*' || currentChar === '/') {
+            operInputStarted = true
+            currentOperArr.push(currentChar)
+        } else if (currentChar === ' ') {
+            // if the char is a space, continue the loop and do nothing, 
+            continue
+        }else if (!operInputStarted && parseInt(currentChar)) {
+            // if current char is a number and operInputStarted is false
+            currentNumArr.push(parseInt(currentChar))
+        } else if (operInputStarted && !parseInt(currentChar)) {
+            // operInputStarted is true, more none numbers operator inputs 
+            currentOperArr.push(currentChar)
+        } else if (operInputStarted && parseInt(currentChar)) {
+            // if operIuptuStarted is true and current char is a number. reset the currentCharArr and currentOperArr. remind user to retype the input. 
+            // break current loop, go to the commmand line input prompt
+            currentNumArr = []
+            currentOperArr = []
+            console.log('This is a postFix calculator, the operators must follow numbers. Please check your numbers/operators order and re-type.')
+            break
+
         } else {
-            throw new Error('Input data must be valid number or operators')
+            // concat the current arrays into the final arrays for arithmetic operations. 
+            // how to invoke the calcLogic? 
         }
+        
+        // need a condition to return true or flase, whihc will decide if invoke caclLogic
     }
-    */
-   
+    console.log(`current number: ${currentNumArr}; current oper: ${currentOperArr}`)
+
+    // if operators more than numbers could perform, ask user retype - wip. 
+}
+
+const calcLogic = function(inputStr) {
    // concat current input numbers and operators with numArr and operArr 
    numArr = numArr.concat(currentNumArr)
    operArr = operArr.concat(currentOperArr)
@@ -97,50 +128,6 @@ const calcUtility = function(cmdLineStr) {
         console.log(userInstruction)
     }
 }
-
-let currentNumArr = [] 
-let currentOperArr = []
-const inputStrValidation = function(cmdLineStr) {
-    // if str operators ahead of numbers in the string, ask user to re-type
-    // need counter to track when the numbers trun into operator 
-    let operInputStarted = false
-    // need the index 
-    for( let i = 0; i < cmdLineStr.length; i ++) {
-        // how to know if the operInput started? 
-        let currentChar = cmdLineStr.charAt(i)
-
-        if (currentChar === '+' || currentChar === '-' || currentChar === '*' || currentChar === '/') {
-            operInputStarted = true
-            currentOperArr.push(currentChar)
-        } else if (currentChar === ' ') {
-            // if the char is a space, continue the loop and do nothing, 
-            continue
-        }else if (!operInputStarted && parseInt(currentChar)) {
-            // if current char is a number and operInputStarted is false
-            currentNumArr.push(parseInt(currentChar))
-        } else if (operInputStarted && !parseInt(currentChar)) {
-            // operInputStarted is true, more none numbers operator inputs 
-            currentOperArr.push(currentChar)
-        } else if (operInputStarted && parseInt(currentChar)) {
-            // if operIuptuStarted is true and current char is a number. reset the currentCharArr and currentOperArr. remind user to retype the input. 
-            // break current loop, go to the commmand line input prompt
-            currentNumArr = []
-            currentOperArr = []
-            console.log('This is a postFix calculator, the operators must follow numbers. Please check your numbers/operators order and re-type.')
-            break
-
-        } else {
-            // concat the current arrays into the final arrays for arithmetic operations. 
-            // how to invoke the calcLogic? 
-        }
-        
-        // need a condition to return true or flase, whihc will decide if invoke caclLogic
-    }
-    console.log(`current number: ${currentNumArr}; current oper: ${currentOperArr}`)
-
-    // if operators more than numbers could perform, ask user retype - wip. 
-}
-
 
 // async solution to manage cmd-line input and exit program
 const rl = readline.createInterface({
