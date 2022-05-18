6e4e776ea9f6ac39b8a9447a8bfe94617fd65afe
hankeliu2015 <hanke.liu@gmail.com>
2022-05-18 12:13:54 -0400

    remove duplicate conditions and validation logic in calcLogic function

--- utilities/operLimitValidation.js
+++ utilities/operLimitValidation.js
@@ -1,9 +1,9 @@
-module.exports = operLimitValidation = function(currentNumArr, currentOperArr, numArr, operArr) {
+module.exports = operLimitValidation = function(currentNumArr, currentOperArr, numArr) {
     // validate the operators meet the limit. 
     // The limit is operators length should be less than numbers length - 1
     // If the limit is passed, empty the current arrays and ask user to correct. 
     
-    if (currentOperArr.length > (numArr.length - 1)) {
+    if (currentOperArr.length > (numArr.length + currentNumArr.length - 1)) {
         // undo the current arrays concatenation for numArr and operArr
         // use the current arrays length, remove the last els concatenated with numArr and operArr
         // I might not need to do the undo becuase the concatenate has not happen yet. 
@@ -15,11 +15,11 @@ module.exports = operLimitValidation = function(currentNumArr, currentOperArr, n
         //     operArr.splice(-currentOperArr.length)
         // }
 
-        // reset current number array and oper Arr. 
+        console.log(`2nd validation - Input operators are over the limit. The total operators should be one less than the input numbers plus previous numbers. Current input data is reset and previous input data is saved. please re-enter valid numbers and operators`)
+        console.log(`curent oper length: ${currentOperArr.length}`)
         currentNumArr = []
         currentOperArr = []
-
-        console.log(`2nd validation - Input operators are over the limit. The total operators should be one less than the input numbers plus previous numbers. Current input data is reset and previous input data is saved. please re-enter valid numbers and operators`)
+        console.log(`curent oper length after reset: ${currentOperArr.length}`)
     } 
     console.log('2nd validation - end')
 }
